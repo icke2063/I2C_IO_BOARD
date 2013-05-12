7c807eb8c8c7d24285e1eaabe78acc54f4dd8d09
icke <icke@X230iSSD>
2013-05-12 20:57:47 +0200

    update slave_config

--- base/config/slave_config.h
+++ base/config/slave_config.h
@@ -24,75 +24,10 @@
 #ifndef _SLAVE_CONFIG_H_
 #define _SLAVE_CONFIG_H_
 
-#define SLAVE_ADRESSE 0x51
+#define	SLAVE_ID	0x2A
+#define DEFAULT_SLAVE_ADRESSE 0x51
 
-#define MAX_IO_PINS 12
-#define MAX_IO_PIN_NAME_LENGTH	15
-
-
-/**
-*intern IO Port mapping
-* - P_PORT_OI_?: pointer to port register
-* - P_PIN_IO_? : pointer to pin register
-* - P_DDR_IO_? : pointer to ddr register
-* - PIN_IO_?   : pin of associated port
-*
-*/
-#define P_PORT_IO_0	&PORTC
-#define P_PORT_IO_1	&PORTC
-#define P_PORT_IO_2	&PORTC
-#define P_PORT_IO_3	&PORTC
-#define P_PORT_IO_4	&PORTD
-#define P_PORT_IO_5	&PORTD
-
-#define P_PORT_IO_6	&PORTD
-#define P_PORT_IO_7	&PORTD
-#define P_PORT_IO_8	&PORTD
-#define P_PORT_IO_9	&PORTD
-#define P_PORT_IO_A	&PORTD
-#define P_PORT_IO_B	&PORTD
-
-#define P_DDR_IO_0	&DDRC
-#define P_DDR_IO_1	&DDRC
-#define P_DDR_IO_2	&DDRC
-#define P_DDR_IO_3	&DDRC
-#define P_DDR_IO_4	&DDRD
-#define P_DDR_IO_5	&DDRD
-
-#define P_DDR_IO_6	&DDRD
-#define P_DDR_IO_7	&DDRD
-#define P_DDR_IO_8	&DDRD
-#define P_DDR_IO_9	&DDRD
-#define P_DDR_IO_A	&DDRD
-#define P_DDR_IO_B	&DDRD
-
-#define P_PIN_IO_0	&PINC
-#define P_PIN_IO_1	&PINC
-#define P_PIN_IO_2	&PINC
-#define P_PIN_IO_3	&PINC
-#define P_PIN_IO_4	&PIND
-#define P_PIN_IO_5	&PIND
-
-#define P_PIN_IO_6	&PIND
-#define P_PIN_IO_7	&PIND
-#define P_PIN_IO_8	&PIND
-#define P_PIN_IO_9	&PIND
-#define P_PIN_IO_A	&PIND
-#define P_PIN_IO_B	&PIND
-
-#define PIN_IO_0	1
-#define PIN_IO_1	3
-#define PIN_IO_2	2
-#define PIN_IO_3	0
-#define PIN_IO_4	0
-#define PIN_IO_5	1
-
-#define PIN_IO_6	2
-#define PIN_IO_7	3
-#define PIN_IO_8	4
-#define PIN_IO_9	5
-#define PIN_IO_A	6
-#define PIN_IO_B	7
+#define COUNT_IO_PINS 16
 
 
 #endif //_SLAVE_CONFIG_H
