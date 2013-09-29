befe3e5439120f9c691f50dfea0d151fdcbb4e49
icke2063 <icke@X230iSSD>
2013-09-29 13:28:53 +0200

    add default virtual io pin header file

---
+++ base/inc/default_iopin.h
@@ -0,0 +1,263 @@
+/**
+ * @file   default_viopin.h
+ * @Author icke
+ * @date   29.09.2013
+ * @brief  Brief description of file.
+ *
+ * Detailed description of file.
+ */
+
+#ifndef DEFAULT_VIOPIN_H_
+#define DEFAULT_VIOPIN_H_
+
+#include <stddef.h>
+
+/**
+ * uart pin addressing
+ */
+
+
+/**
+ * create default defines for pin addressing
+ */
+
+///UART_TX
+#ifndef UARTTX_PORT
+	#define UARTTX_PORT	NULL
+#endif
+#ifndef UARTTX_PIN
+	#define UARTTX_PIN		NULL
+#endif
+#ifndef UARTTX_DDR
+	#define UARTTX_DDR	NULL
+#endif
+#ifndef UARTTX_PINNR
+	#define UARTTX_PINNR	0
+#endif
+
+
+///PIN _0
+#ifndef VIO_PORT_0
+	#define VIO_PORT_0	NULL
+#endif
+#ifndef VIO_PIN_0
+	#define VIO_PIN_0	NULL
+#endif
+#ifndef VIO_DDR_0
+	#define VIO_DDR_0	NULL
+#endif
+#ifndef VIO_PINNR_0
+	#define VIO_PINNR_0	0
+#endif
+
+///PIN _1
+#ifndef VIO_PORT_1
+	#define VIO_PORT_1	NULL
+#endif
+#ifndef VIO_PIN_1
+	#define VIO_PIN_1	NULL
+#endif
+#ifndef VIO_DDR_1
+	#define VIO_DDR_1	NULL
+#endif
+#ifndef VIO_PINNR_1
+	#define VIO_PINNR_1	0
+#endif
+
+///PIN _2
+#ifndef VIO_PORT_2
+	#define VIO_PORT_2	NULL
+#endif
+#ifndef VIO_PIN_2
+	#define VIO_PIN_2	NULL
+#endif
+#ifndef VIO_DDR_2
+	#define VIO_DDR_2	NULL
+#endif
+#ifndef VIO_PINNR_2
+	#define VIO_PINNR_2	0
+#endif
+
+///PIN _3
+#ifndef VIO_PORT_3
+	#define VIO_PORT_3	NULL
+#endif
+#ifndef VIO_PIN_3
+	#define VIO_PIN_3	NULL
+#endif
+#ifndef VIO_DDR_3
+	#define VIO_DDR_3	NULL
+#endif
+#ifndef VIO_PINNR_3
+	#define VIO_PINNR_3	0
+#endif
+
+///PIN _4
+#ifndef VIO_PORT_4
+	#define VIO_PORT_4	NULL
+#endif
+#ifndef VIO_PIN_4
+	#define VIO_PIN_4	NULL
+#endif
+#ifndef VIO_DDR_4
+	#define VIO_DDR_4	NULL
+#endif
+#ifndef VIO_PINNR_4
+	#define VIO_PINNR_4	0
+#endif
+
+///PIN _5
+#ifndef VIO_PORT_5
+	#define VIO_PORT_5	NULL
+#endif
+#ifndef VIO_PIN_5
+	#define VIO_PIN_5	NULL
+#endif
+#ifndef VIO_DDR_5
+	#define VIO_DDR_5	NULL
+#endif
+#ifndef VIO_PINNR_5
+	#define VIO_PINNR_5	0
+#endif
+
+///PIN _6
+#ifndef VIO_PORT_6
+	#define VIO_PORT_6	NULL
+#endif
+#ifndef VIO_PIN_6
+	#define VIO_PIN_6	NULL
+#endif
+#ifndef VIO_DDR_6
+	#define VIO_DDR_6	NULL
+#endif
+#ifndef VIO_PINNR_6
+	#define VIO_PINNR_6	0
+#endif
+
+///PIN _7
+#ifndef VIO_PORT_7
+	#define VIO_PORT_7	NULL
+#endif
+#ifndef VIO_PIN_7
+	#define VIO_PIN_7	NULL
+#endif
+#ifndef VIO_DDR_7
+	#define VIO_DDR_7	NULL
+#endif
+#ifndef VIO_PINNR_7
+	#define VIO_PINNR_7	0
+#endif
+
+///PIN _8
+#ifndef VIO_PORT_8
+	#define VIO_PORT_8	NULL
+#endif
+#ifndef VIO_PIN_8
+	#define VIO_PIN_8	NULL
+#endif
+#ifndef VIO_DDR_8
+	#define VIO_DDR_8	NULL
+#endif
+#ifndef VIO_PINNR_8
+	#define VIO_PINNR_8	0
+#endif
+
+///PIN _9
+#ifndef VIO_PORT_9
+	#define VIO_PORT_9	NULL
+#endif
+#ifndef VIO_PIN_9
+	#define VIO_PIN_9	NULL
+#endif
+#ifndef VIO_DDR_9
+	#define VIO_DDR_9	NULL
+#endif
+#ifndef VIO_PINNR_9
+	#define VIO_PINNR_9	0
+#endif
+
+///PIN _A
+#ifndef VIO_PORT_A
+	#define VIO_PORT_A	NULL
+#endif
+#ifndef VIO_PIN_A
+	#define VIO_PIN_A	NULL
+#endif
+#ifndef VIO_DDR_A
+	#define VIO_DDR_A	NULL
+#endif
+#ifndef VIO_PINNR_A
+	#define VIO_PINNR_A	0
+#endif
+
+///PIN _B
+#ifndef VIO_PORT_B
+	#define VIO_PORT_B	NULL
+#endif
+#ifndef VIO_PIN_B
+	#define VIO_PIN_B	NULL
+#endif
+#ifndef VIO_DDR_B
+	#define VIO_DDR_B	NULL
+#endif
+#ifndef VIO_PINNR_B
+	#define VIO_PINNR_B	0
+#endif
+
+///PIN _C
+#ifndef VIO_PORT_C
+	#define VIO_PORT_C	NULL
+#endif
+#ifndef VIO_PIN_C
+	#define VIO_PIN_C	NULL
+#endif
+#ifndef VIO_DDR_C
+	#define VIO_DDR_C	NULL
+#endif
+#ifndef VIO_PINNR_C
+	#define VIO_PINNR_C	0
+#endif
+
+///PIN _D
+#ifndef VIO_PORT_D
+	#define VIO_PORT_D	NULL
+#endif
+#ifndef VIO_PIN_D
+	#define VIO_PIN_D	NULL
+#endif
+#ifndef VIO_DDR_D
+	#define VIO_DDR_D	NULL
+#endif
+#ifndef VIO_PINNR_D
+	#define VIO_PINNR_D	0
+#endif
+
+///PIN _E
+#ifndef VIO_PORT_E
+	#define VIO_PORT_E	NULL
+#endif
+#ifndef VIO_PIN_E
+	#define VIO_PIN_E	NULL
+#endif
+#ifndef VIO_DDR_E
+	#define VIO_DDR_E	NULL
+#endif
+#ifndef VIO_PINNR_E
+	#define VIO_PINNR_E	0
+#endif
+
+///PIN _F
+#ifndef VIO_PORT_F
+	#define VIO_PORT_F	NULL
+#endif
+#ifndef VIO_PIN_F
+	#define VIO_PIN_F	NULL
+#endif
+#ifndef VIO_DDR_F
+	#define VIO_DDR_F	NULL
+#endif
+#ifndef VIO_PINNR_F
+	#define VIO_PINNR_F	0
+#endif
+
+#endif /* DEFAULT_VIOPIN_H_ */
